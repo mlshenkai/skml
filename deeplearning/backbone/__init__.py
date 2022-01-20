6f24f3346500260f56b6787b951124ce9650a48b
shenkai <123456789kY>
2022-01-20 21:45:27 +0800

    hrnet module

--- deeplearning/backbone/__init__.py
+++ deeplearning/backbone/__init__.py
@@ -3,3 +3,8 @@
 # @Created Time: 2022/1/19 5:54 PM
 # @Organization: YQN
 # @Email: mlshenkai@163.com
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+from hrnet import HighResolutionModule
--- deeplearning/backbone/hrnet.py
+++ deeplearning/backbone/hrnet.py
@@ -7,7 +7,11 @@
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
+
+import collections
 import torch.nn as nn
+import os
+import torch
 
 BN_MOMENTUM = 0.1
 
@@ -291,3 +295,247 @@ class HighResolutionModule(nn.Module):
         return x_fuse
 
 
+blocks_dict = {"BASIC": BasicBlock, "BOTTLENECK": Bottleneck}
+
+
+class PoseHighResolutionNet(nn.Module):
+    def __init__(self, cfg, **kwargs):
+        self.kwargs = kwargs
+        self.in_planes = 64
+        extra = cfg["MODEL"]["EXTRA"]
+        super(PoseHighResolutionNet, self).__init__()
+
+        # stem net
+        self.conv1 = nn.Conv2d(
+            3, 64, kernel_size=(3, 3), stride=(2, 2), padding=(1, 1), bias=False
+        )
+        self.bn1 = nn.BatchNorm2d(64, momentum=BN_MOMENTUM)
+        self.conv2 = nn.Conv2d(
+            64, 64, kernel_size=(3, 3), stride=(2, 2), padding=(1, 1), bias=False
+        )
+        self.bn2 = nn.BatchNorm2d(64, momentum=BN_MOMENTUM)
+        self.relu = nn.ReLU(inplace=True)
+        self.layer1 = self._make_layer(Bottleneck, 64, 4)
+
+        self.stage2_cfg = extra["STAGE2"]
+        num_channels = self.stage2_cfg["NUM_CHANNELS"]
+        block = blocks_dict[self.stage2_cfg["BLOCK"]]
+        num_channels = [
+            num_channels[i] * block.expansion for i in range(len(num_channels))
+        ]
+        self.transition1 = self._make_transition_layer([256], num_channels)
+        self.stage2, pre_stage_channels = self._make_stage(
+            self.stage2_cfg, num_channels
+        )
+
+        self.stage3_cfg = extra["STAGE3"]
+        num_channels = self.stage3_cfg["NUM_CHANNELS"]
+        block = blocks_dict[self.stage3_cfg["BLOCK"]]
+        num_channels = [
+            num_channels[i] * block.expansion for i in range(len(num_channels))
+        ]
+        self.transition2 = self._make_transition_layer(pre_stage_channels, num_channels)
+        self.stage3, pre_stage_channels = self._make_stage(
+            self.stage3_cfg, num_channels
+        )
+
+        self.stage4_cfg = extra["STAGE4"]
+        num_channels = self.stage4_cfg["NUM_CHANNELS"]
+        block = blocks_dict[self.stage4_cfg["BLOCK"]]
+        num_channels = [
+            num_channels[i] * block.expansion for i in range(len(num_channels))
+        ]
+        self.transition3 = self._make_transition_layer(pre_stage_channels, num_channels)
+        self.stage4, pre_stage_channels = self._make_stage(
+            self.stage4_cfg, num_channels, multi_scale_output=False
+        )
+
+        self.final_layer = nn.Conv2d(
+            in_channels=pre_stage_channels[0],
+            out_channels=cfg["MODEL"]["NUM_JOINTS"],
+            kernel_size=extra["FINAL_CONV_KERNEL"],
+            stride=(1, 1),
+            padding=(1, 1) if extra["FINAL_CONV_KERNEL"] == 3 else (0, 0),
+        )
+
+        self.pretrained_layers = extra["PRETRAINED_LAYERS"]
+
+    @staticmethod
+    def _make_transition_layer(num_channels_pre_layer, num_channels_cur_layer):
+        num_branches_cur = len(num_channels_cur_layer)
+        num_branches_pre = len(num_channels_pre_layer)
+
+        transition_layers = []
+        for i in range(num_branches_cur):
+            if i < num_branches_pre:
+                if num_channels_cur_layer[i] != num_channels_pre_layer[i]:
+                    transition_layers.append(
+                        nn.Sequential(
+                            nn.Conv2d(
+                                num_channels_pre_layer[i],
+                                num_channels_cur_layer[i],
+                                kernel_size=(3, 3),
+                                stride=(1, 1),
+                                padding=(1, 1),
+                                bias=False,
+                            ),
+                            nn.BatchNorm2d(num_channels_cur_layer[i]),
+                            nn.ReLU(inplace=True),
+                        )
+                    )
+                else:
+                    transition_layers.append(None)
+            else:
+                conv3x3s = []
+                for j in range(i + 1 - num_branches_pre):
+                    in_channels = num_channels_pre_layer[-1]
+                    out_channels = (
+                        num_channels_cur_layer[i]
+                        if j == i - num_branches_pre
+                        else in_channels
+                    )
+                    conv3x3s.append(
+                        nn.Sequential(
+                            nn.Conv2d(
+                                in_channels,
+                                out_channels,
+                                kernel_size=(3, 3),
+                                stride=(2, 2),
+                                padding=(1, 1),
+                                bias=False,
+                            ),
+                            nn.BatchNorm2d(out_channels),
+                            nn.ReLU(inplace=True),
+                        )
+                    )
+                transition_layers.append(nn.Sequential(*conv3x3s))
+
+        return nn.ModuleList(transition_layers)
+
+    def _make_layer(self, block, planes, blocks, stride=1):
+        down_sample = None
+        if stride != 1 or self.in_planes != planes * block.expansion:
+            down_sample = nn.Sequential(
+                nn.Conv2d(
+                    self.in_planes,
+                    planes * block.expansion,
+                    kernel_size=(1, 1),
+                    stride=(stride, stride),
+                    bias=False,
+                ),
+                nn.BatchNorm2d(planes * block.expansion, momentum=BN_MOMENTUM),
+            )
+
+        layers = [block(self.in_planes, planes, stride, down_sample)]
+        self.in_planes = planes * block.expansion
+        for i in range(1, blocks):
+            layers.append(block(self.in_planes, planes))
+
+        return nn.Sequential(*layers)
+
+    @staticmethod
+    def _make_stage(layer_config, num_in_channels, multi_scale_output=True):
+        num_modules = layer_config["NUM_MODULES"]
+        num_branches = layer_config["NUM_BRANCHES"]
+        num_blocks = layer_config["NUM_BLOCKS"]
+        num_channels = layer_config["NUM_CHANNELS"]
+        block = blocks_dict[layer_config["BLOCK"]]
+        fuse_method = layer_config["FUSE_METHOD"]
+
+        modules = []
+        for i in range(num_modules):
+            # multi_scale_output is only used last module
+            if not multi_scale_output and i == num_modules - 1:
+                reset_multi_scale_output = False
+            else:
+                reset_multi_scale_output = True
+
+            modules.append(
+                HighResolutionModule(
+                    num_branches,
+                    block,
+                    num_blocks,
+                    num_in_channels,
+                    num_channels,
+                    fuse_method,
+                    reset_multi_scale_output,
+                )
+            )
+            num_in_channels = modules[-1].get_num_inchannels()
+
+        return nn.Sequential(*modules), num_in_channels
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+        x = self.conv2(x)
+        x = self.bn2(x)
+        x = self.relu(x)
+        x = self.layer1(x)
+
+        x_list = []
+        for i in range(self.stage2_cfg["NUM_BRANCHES"]):
+            if self.transition1[i] is not None:
+                x_list.append(self.transition1[i](x))
+            else:
+                x_list.append(x)
+        y_list = self.stage2(x_list)
+
+        x_list = []
+        for i in range(self.stage3_cfg["NUM_BRANCHES"]):
+            if self.transition2[i] is not None:
+                x_list.append(self.transition2[i](y_list[-1]))
+            else:
+                x_list.append(y_list[i])
+        y_list = self.stage3(x_list)
+
+        x_list = []
+        for i in range(self.stage4_cfg["NUM_BRANCHES"]):
+            if self.transition3[i] is not None:
+                x_list.append(self.transition3[i](y_list[-1]))
+            else:
+                x_list.append(y_list[i])
+        y_list = self.stage4(x_list)
+
+        x = self.final_layer(y_list[0])
+
+        return x
+
+    def init_weights(self, pretrained=""):
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.normal_(m.weight, std=0.001)
+                for name, _ in m.named_parameters():
+                    if name in ["bias"]:
+                        nn.init.constant_(m.bias, 0)
+            elif isinstance(m, nn.BatchNorm2d):
+                nn.init.constant_(m.weight, 1)
+                nn.init.constant_(m.bias, 0)
+            elif isinstance(m, nn.ConvTranspose2d):
+                nn.init.normal_(m.weight, std=0.001)
+                for name, _ in m.named_parameters():
+                    if name in ["bias"]:
+                        nn.init.constant_(m.bias, 0)
+
+        if os.path.isfile(pretrained):
+            pretrained_state_dict = torch.load(pretrained)
+            need_init_state_dict = collections.OrderedDict()
+            for name, m in pretrained_state_dict.items():
+                if (
+                    name.split(".")[0] in self.pretrained_layers
+                    or self.pretrained_layers[0] is "*"
+                ):
+                    need_init_state_dict[name] = m
+            self.load_state_dict(need_init_state_dict, strict=False)
+        elif pretrained:
+            raise ValueError("{} is not exist!".format(pretrained))
+
+
+def get_pose_net(cfg, is_train, **kwargs):
+    model = PoseHighResolutionNet(cfg, **kwargs)
+
+    if is_train and cfg["MODEL"]["INIT_WEIGHTS"]:
+        model.init_weights(cfg["MODEL"]["PRETRAINED"])
+
+    return model
